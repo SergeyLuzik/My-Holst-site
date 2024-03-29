12520fec77ac63340d7384e10b6b48c941eaeb80
Сергей Лузик <67701136+SergeyLuzik@users.noreply.github.com>
2024-03-29 05:45:00 +0000

    feat: block js slider on less then 1130px

--- index.js
+++ index.js
@@ -1,83 +1,84 @@
 // HERO FEEDBACK SLIDER
+if (document.querySelector(".page-main").offsetWidth > 1130) {
+  (() => {
+    const slidesWrapper = document.querySelector(".slider__slides-list"),
+      gapValue = parseInt(getComputedStyle(slidesWrapper).gap),
+      slides = slidesWrapper.children,
+      slideWidth = slides[0].offsetWidth,
+      offset = slideWidth + gapValue,
+      transitionTime = "0.3s",
+      slideScale = "translate(-10%, 6.5%) scale(1.2)",
+      left = document.querySelector(".slider-button_left"),
+      right = document.querySelector(".slider-button_right");
 
-(() => {
-  const slidesWrapper = document.querySelector(".slider__slides-list"),
-    gapValue = parseInt(getComputedStyle(slidesWrapper).gap),
-    slides = slidesWrapper.children,
-    slideWidth = slides[0].offsetWidth,
-    offset = slideWidth + gapValue,
-    transitionTime = "0.3s",
-    slideScale = "translate(-10%, 6.5%) scale(1.2)",
-    left = document.querySelector(".slider-button_left"),
-    right = document.querySelector(".slider-button_right");
+    let inAction = false;
 
-  let inAction = false;
+    // Initialize slider start position
+    slidesWrapper.style.transform = `translateX(${-offset}px)`;
+    slides[0].style.opacity = 0;
+    //slides[0].style.transform = "translate(0) scale(0.3)";
+    slides[1].style.transform = slideScale;
+    //slides[0].style.transitionTimingFunction = "ease-in";
 
-  // Initialize slider start position
-  slidesWrapper.style.transform = `translateX(${-offset}px)`;
-  slides[0].style.opacity = 0;
-  //slides[0].style.transform = "translate(0) scale(0.3)";
-  slides[1].style.transform = slideScale;
-  //slides[0].style.transitionTimingFunction = "ease-in";
+    function movLeft(slidesWrapper, offset) {
+      slidesWrapper.ontransitionend = (_) => {
+        slidesWrapper.ontransitionend = null;
+        slidesWrapper.style.transition = "none";
+        slides[0].removeAttribute("style");
 
-  function movLeft(slidesWrapper, offset) {
-    slidesWrapper.ontransitionend = (_) => {
-      slidesWrapper.ontransitionend = null;
-      slidesWrapper.style.transition = "none";
-      slides[0].removeAttribute("style");
+        slidesWrapper.append(slides[0]);
+        slidesWrapper.style.transform = `translateX(${-offset}px)`;
 
-      slidesWrapper.append(slides[0]);
-      slidesWrapper.style.transform = `translateX(${-offset}px)`;
+        inAction = false;
+      };
+      slidesWrapper.style.transition = transitionTime; //todo задавать transition через запятую всем сразу?
+      slidesWrapper.style.transform = `translateX(${-offset * 2}px)`;
 
-      inAction = false;
+      slides[1].style.transition = transitionTime;
+      slides[1].style.opacity = 0;
+      slides[1].style.transform = "translate(0)"; // scale(0.3)
+
+      slides[2].style.transition = transitionTime;
+      slides[2].style.transform = slideScale;
+      slides[2].style.transitionTimingFunction = "cubic-bezier(.72,.07,.95,.7)"; //.75,0,1,.02 ease-in
+    }
+    function movRight(slidesWrapper, offset) {
+      slidesWrapper.ontransitionend = (_) => {
+        slidesWrapper.ontransitionend = null;
+        slidesWrapper.style.transition = "none";
+        slides[1].removeAttribute("style");
+
+        slidesWrapper.lastElementChild.style.opacity = 0;
+        slidesWrapper.prepend(slidesWrapper.lastElementChild);
+        slidesWrapper.style.transform = `translateX(${-offset}px)`;
+
+        inAction = false;
+      };
+      slidesWrapper.style.transition = transitionTime;
+      slidesWrapper.style.transform = `translateX(0px)`;
+
+      slides[0].style.transition = transitionTime;
+      slides[0].style.opacity = 1;
+      slides[0].style.transform = slideScale;
+      slides[0].style.transitionTimingFunction = "ease-in";
+
+      slides[1].style.transition = transitionTime;
+      slides[1].style.transform = "translate(0) scale(1)";
+      slides[1].style.transitionTimingFunction = "cubic-bezier(0,.75,.02,1)"; //0,.75,.02,1  // 0,0,.02,1 // .75,0,1,.02
+    }
+
+    left.onclick = (_) => {
+      if (inAction) return;
+      inAction = true;
+      movRight(slidesWrapper, offset);
     };
-    slidesWrapper.style.transition = transitionTime; //todo задавать transition через запятую всем сразу?
-    slidesWrapper.style.transform = `translateX(${-offset * 2}px)`;
-
-    slides[1].style.transition = transitionTime;
-    slides[1].style.opacity = 0;
-    slides[1].style.transform = "translate(0)"; // scale(0.3)
-
-    slides[2].style.transition = transitionTime;
-    slides[2].style.transform = slideScale;
-    slides[2].style.transitionTimingFunction = "cubic-bezier(.72,.07,.95,.7)"; //.75,0,1,.02 ease-in
-  }
-  function movRight(slidesWrapper, offset) {
-    slidesWrapper.ontransitionend = (_) => {
-      slidesWrapper.ontransitionend = null;
-      slidesWrapper.style.transition = "none";
-      slides[1].removeAttribute("style");
-
-      slidesWrapper.lastElementChild.style.opacity = 0;
-      slidesWrapper.prepend(slidesWrapper.lastElementChild);
-      slidesWrapper.style.transform = `translateX(${-offset}px)`;
-
-      inAction = false;
+    right.onclick = (_) => {
+      if (inAction) return;
+      inAction = true;
+      movLeft(slidesWrapper, offset);
     };
-    slidesWrapper.style.transition = transitionTime;
-    slidesWrapper.style.transform = `translateX(0px)`;
-
-    slides[0].style.transition = transitionTime;
-    slides[0].style.opacity = 1;
-    slides[0].style.transform = slideScale;
-    slides[0].style.transitionTimingFunction = "ease-in";
-
-    slides[1].style.transition = transitionTime;
-    slides[1].style.transform = "translate(0) scale(1)";
-    slides[1].style.transitionTimingFunction = "cubic-bezier(0,.75,.02,1)"; //0,.75,.02,1  // 0,0,.02,1 // .75,0,1,.02
-  }
-
-  left.onclick = (_) => {
-    if (inAction) return;
-    inAction = true;
-    movRight(slidesWrapper, offset);
-  };
-  right.onclick = (_) => {
-    if (inAction) return;
-    inAction = true;
-    movLeft(slidesWrapper, offset);
-  };
-})();
+  })();
+}
 
 // FEEDBACK SLIDER
 
