cc62db6eb3a5a2e9bb1fb7e2c59e877b752513dc
Sergey Luzik <luziksergey@gmail.com>
2024-04-13 05:53:28 +0300

    ref: delete defer from ymaps script

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,27 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  node: {
-    __dirname: true,
-  },
-  mode: "development",
   devtool: "inline-source-map",
   entry: "./index.js",
   output: {
     filename: "build.[contenthash].js",
-    path: path.resolve("dist"),
   },
+
   plugins: [
     new MiniCssExtractPlugin({
       filename: "build.[contenthash].css",
     }),
     new HtmlWebpackPlugin({
-      template: "./index.html",
-      filename: "build.[contenthash].html",
+      template: "index.html",
     }),
   ],
-  devServer: {
-    allowedHosts: "all",
-    historyApiFallback: true,
-    static: path.join(__dirname, "dist"),
-    open: true, // Автоматически открывать браузер
-  },
+
   module: {
     rules: [
+      /*{
+        test: /\.html$/,
+        loader: "html-loader",
+      },*/
       {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
