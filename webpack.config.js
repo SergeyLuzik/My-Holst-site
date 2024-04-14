db6771217275adf2523affc82bc43678696d9f83
Sergey Luzik <luziksergey@gmail.com>
2024-04-14 20:21:21 +0300

    feat: add assets processing

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,36 @@ const __dirname = path.dirname(__filename);*/
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
+/*let test = path.resolve("dist", "assets");
+console.log(test);**/
 export default {
   /*  node: {
     __dirname: true,
   },*/
   mode: "development",
   devtool: "inline-source-map",
-  entry: "./index.js",
+  entry: path.resolve("index.js"),
   output: {
     filename: "build.[contenthash].js",
     path: path.resolve("dist"),
+    assetModuleFilename: "images/[name].[hash][ext]",
     publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
@@ -36,14 +55,5 @@ export default {
     open: true,
     hot: true,
     historyApiFallback: true,
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-    ],
   },
 };
