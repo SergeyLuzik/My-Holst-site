5f7390b4e32f38fafd32ea253f1c5219a7e3aaf2
Sergey Luzik <luziksergey@gmail.com>
2024-04-25 06:22:00 +0300

    ref: add cache to webpack dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ import common from "./webpack.common.js";
 export default merge(common, {
   mode: "development",
   devtool: "inline-source-map",
+  cache: true,
 
   devServer: {
     allowedHosts: "all",
