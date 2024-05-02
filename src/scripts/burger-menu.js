993956c3863b73494616453e3f5729eb94da4f6a
Sergey Luzik <sergeyluzik@gmail.com>
2024-05-02 13:58:53 +0300

    ref: change type of burger button listener

--- src/scripts/burger-menu.js
+++ src/scripts/burger-menu.js
@@ -15,43 +15,49 @@ if (getMainWidth() <= 1250) {
     };
   }
 
-  burgerButton.onclick = (e) => {
-    console.log("клик на кнопку в обработчике кнопки");
-    const menu =
-      getMainWidth() <= 680
-        ? document.querySelector(".page-header__menu")
-        : document.querySelector(".page-header__nav");
-    menu.style.top = `${document.querySelector(".page-header").offsetHeight}px`;
-    /*e.stopPropagation(); // todo такое себе решение (при клике на кнопку вешать обработчик на document и уже в нем обрабатывать и кнопку и тап по ссылке и окну?)
+  burgerButton.addEventListener(
+    "click",
+    (e) => {
+      console.log("клик на кнопку в обработчике кнопки");
+      const menu =
+        getMainWidth() <= 680
+          ? document.querySelector(".page-header__menu")
+          : document.querySelector(".page-header__nav");
+      menu.style.top = `${
+        document.querySelector(".page-header").offsetHeight
+      }px`;
+      /*e.stopPropagation(); // todo такое себе решение (при клике на кнопку вешать обработчик на document и уже в нем обрабатывать и кнопку и тап по ссылке и окну?)
     burgerButton.classList.toggle("page-header__burger-button_open"); // вынести в функцию menuToggle
     document.documentElement.classList.toggle("stop-scroll");
     menu.classList.toggle(`${menu.classList[0]}_open`);*/
-    burgerButton.onclick = null;
-    document.addEventListener(
-      "click",
-      (e) => {
-        console.log("событие click в обработчике документа");
-        if (
-          !e.target.closest(".page-header__burger-button") &&
-          !menu.classList.contains(`${menu.classList[0]}_open`)
-        ) {
-          console.log("клик не на кнопку при закрытом меню");
-          return;
-        }
-        // todo при клике после открытия срабатывает со 2го раза
-        burgerButton.classList.toggle("page-header__burger-button_open");
-        document.documentElement.classList.toggle("stop-scroll");
-        menu.classList.toggle(`${menu.classList[0]}_open`);
+      //burgerButton.onclick = null;
+      document.addEventListener(
+        "click",
+        (e) => {
+          console.log("событие click в обработчике документа");
+          if (
+            !e.target.closest(".page-header__burger-button") &&
+            !menu.classList.contains(`${menu.classList[0]}_open`)
+          ) {
+            console.log("клик не на кнопку при закрытом меню");
+            return;
+          }
+          // todo при клике после открытия срабатывает со 2го раза
+          burgerButton.classList.toggle("page-header__burger-button_open");
+          document.documentElement.classList.toggle("stop-scroll");
+          menu.classList.toggle(`${menu.classList[0]}_open`);
 
-        if (e.target.closest(".page-header__nav-list")) {
-          console.log("клик на ссылку в меню");
-          e.preventDefault();
-          document
-            .querySelector(e.target.getAttribute("href"))
-            .scrollIntoView({ behavior: "smooth" });
+          if (e.target.closest(".page-header__nav-list")) {
+            console.log("клик на ссылку в меню");
+            e.preventDefault();
+            document
+              .querySelector(e.target.getAttribute("href"))
+              .scrollIntoView({ behavior: "smooth" });
+          }
         }
-      }
-      //{ once: true }
-    );
-  };
+        //{ once: true }
+      );
+    },
+    { once: true }
+  );
 }
