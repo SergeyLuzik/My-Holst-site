d32e92d476910d79e04ecb70423e15d706d66676
Sergey Luzik <luziksergey@gmail.com>
2024-05-08 20:34:41 +0300

    ref: start animation before scroll

--- src/scripts/initial-animations.js
+++ src/scripts/initial-animations.js
@@ -36,18 +36,33 @@ window.onload = () => {
         advantage.classList.remove("translate-up-20");
       }, index * 200);
     });*/
-    const animatedElements = document.querySelectorAll(".animate");
+    //const animatedElements = document.querySelectorAll(".animate");
+
+    animateElements();
     window.addEventListener("scroll", () => {
-      animatedElements.forEach((element) => {
+      animateElements();
+      /* animatedElements.forEach((element) => {
         const elementPosition = element.getBoundingClientRect().top;
         const targetPosition = window.innerHeight * 0.8;
         if (elementPosition < targetPosition) {
           element.classList.add("scrolled-in");
         }
-      });
+      });*/
     });
   });
 };
+
+function animateElements() {
+  const elements = document.querySelectorAll(".animate:not(.scrolled-in)");
+  // todo при каждом вызове заново ищутся элементы (обьявить до и передавать, потом проверять в forEach есть ли класс scrolled-in?)
+  elements.forEach((element) => {
+    const elementPosition = element.getBoundingClientRect().top;
+    const targetPosition = window.innerHeight * 0.8;
+    if (elementPosition < targetPosition) {
+      element.classList.add("scrolled-in");
+    }
+  });
+}
 function animNumber(numObj, duration) {
   //const startTime = performance.now(),
   const num = parseInt(numObj.innerHTML.replace(" ", ""), 10);
